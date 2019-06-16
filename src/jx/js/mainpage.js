f6fb1446230a3fa7dbfaf09da84d9c6b5300d402
Visual GMQ <2142587070@qq.com>
2019-06-16 21:49:36 +0800

    update page, changed image

--- src/jx/js/mainpage.js
+++ src/jx/js/mainpage.js
@@ -4,7 +4,45 @@ window.onload = function(){
 	$(".layer1 .toolbar p").slideUp(0);
 	$(".layer1 .toolbar p").slideDown(1000);
 	$(".layer1 .toolbar .cover").fadeOut(0);
+	
+	$(".layer2").fadeOut(0);
+	self.setTimeout(function(){
+		$(".layer2").fadeIn(1000);
+	},0);
 	self.setTimeout(function() {
 		$(".layer1 .toolbar .cover").fadeIn(1500);
 	}, 500);
+	
+	//$(".layer3 .model").css("height", "0px");
+	
+	$(".layer3").slideUp(0);
+	$(".layer3 h1").fadeOut(0);
+	self.setTimeout(function(){
+		$(".layer3").slideDown(1000);
+		$(".layer3 h1").fadeIn(2000);
+	},1000);
+	
+	$(".layer3 .model").css("float", "left");
+	$(".layer3 .model").slideUp(0);
+	self.setTimeout(function(){
+		$(".layer3 .jigou").slideDown(1000);
+	}, 1500);
+	self.setTimeout(function(){
+		//$(".layer3 .shijue").animate({height: '400px'},1000);
+		$(".layer3 .shijue").slideDown(1000);
+	}, 2000);
+	self.setTimeout(function(){
+		//$(".layer3 .kongzhi").animate({height: '400px'},1000);
+		$(".layer3 .kongzhi").slideDown(1000);
+	}, 2500);
+	self.setTimeout(function(){
+		//$(".layer3 .donghua").animate({height: '400px'},1000);
+		$(".layer3 .donghua").slideDown(1000);
+	}, 3000);
+	
+	/*
+	self.setTimeout(function(){
+		$(".layer3 .model").css("float", "none");
+	}, 4000);
+	*/
 }
