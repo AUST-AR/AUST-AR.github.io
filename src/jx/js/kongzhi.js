342ea0d726e9b3ee808a672938d30e382da3b844
Visual GMQ <2142587070@qq.com>
2019-07-27 14:06:32 +0800

    changed folders

--- src/jx/js/kongzhi.js
+++
@@ -1,4 +0,0 @@
-window.onload = function(){
-	Crafty.init(500, 500, document.getElementById("game"));
-	Crafty.e('2D, DOM, Color, Twoway, Canvas').attr({x: 100, y: 100, w: 100, h:100, rotation:45}).color('#F0F').twoway(200);
-}
